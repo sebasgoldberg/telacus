96b8b1dacc4a8affa0b8cadaded92e379db39e06
Juan Sebastian Goldberg <sebas.goldberg@gmail.com>
2018-11-23 11:29:09 -0200

    Primeiro commit desde o template.

---
+++ webapp/Component.js
@@ -0,0 +1,25 @@
+import UIComponent from "sap/ui/core/UIComponent";
+import models from "sap/ui/demo/basicTemplate/model/models";
+
+export default UIComponent.extend("sap.ui.demo.basicTemplate.Component", {
+
+    metadata: {
+        manifest: "json"
+    },
+
+    /**
+     * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+     * @public
+     * @override
+     */
+    init: function() {
+
+        // call the base component's init function
+        UIComponent.prototype.init.apply(this, arguments);
+
+        // set the device model
+        this.setModel(models.createDeviceModel(), "device");
+    }
+
+});
+
