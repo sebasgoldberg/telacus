697ef2cf622c4d12aa5d2a2ad3d66a9ea6fb3c45
Juan Sebastian Goldberg <sebas.goldberg@gmail.com>
2018-11-26 13:20:04 -0200

    Adição da visualização das informações atuais.

--- webapp/controller/TaskDetail.controller.js
+++ webapp/controller/TaskDetail.controller.js
@@ -1,11 +1,18 @@
 import formatter from 'simplifique/telaneg/custos/model/formatter';
+import JSONModel from "sap/ui/model/json/JSONModel";
 
 sap.ui.controller("simplifique.telaneg.custos.controller.TaskDetail", {
 
     formatter: formatter,
 
+    onInit : function () {
+        this.getView().setModel(new JSONModel({
+            ExibirInformacoesAtuaisItems: false,
+            }), 'view_ext');
+    },
+
     onExibirInformacoesAtuais: function() {
-        let m = this.getView().getModel('view');
+        let m = this.getView().getModel('view_ext');
         let bExibirInformacoesAtuaisItems =
             m.getProperty('/ExibirInformacoesAtuaisItems');
         m.setProperty('/ExibirInformacoesAtuaisItems',
