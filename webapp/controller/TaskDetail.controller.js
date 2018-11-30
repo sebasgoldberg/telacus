2e412ea71499f4f8efbaa74362a5c61749879507
Juan Sebastian Goldberg <sebas.goldberg@gmail.com>
2018-11-30 15:45:32 -0200

    Replica de valores de generico para variantes. Desabilitar inputs de compra para derivados e transferidos. Exibição/Ocultamiento de informações atuais. Ocultamiento de colunas de Custo Bruto e Despesas Acessorias em casso da abrangencia não ser a nivel de Centro de Referencia.

--- webapp/controller/TaskDetail.controller.js
+++ webapp/controller/TaskDetail.controller.js
@@ -1,5 +1,6 @@
 import formatter from 'simplifique/telaneg/custos/model/formatter';
 import JSONModel from "sap/ui/model/json/JSONModel";
+import Decimal from "sap/ui/model/odata/type/Decimal";
 
 sap.ui.controller("simplifique.telaneg.custos.controller.TaskDetail", {
 
@@ -18,5 +19,87 @@ sap.ui.controller("simplifique.telaneg.custos.controller.TaskDetail", {
         m.setProperty('/ExibirInformacoesAtuaisItems',
             !bExibirInformacoesAtuaisItems);
     },
+
+    expandRow: function(oRow) {
+        return new Promise( (resolve, reject) => {
+            let v = this.getView();
+            let oTreeTable = v.byId('treeTable');
+            let iRowIndex = oRow.getIndex();
+            if (oTreeTable.isExpanded(iRowIndex))
+                resolve();
+            else{
+                oTreeTable.expand(iRowIndex);
+                let bc = oTreeTable.getContextByIndex(iRowIndex);
+                let oItem = bc.getObject();
+                let oInterval = setInterval( () => {
+                    // Obtemos o contexto da seguinte linha.
+                    let bc = oTreeTable.getContextByIndex(iRowIndex+1);
+                    if (!bc)
+                        return;
+                    let oSeguinteItem = bc.getObject();
+                    if (oSeguinteItem.Pai !== oItem.Item)
+                        return;
+                    resolve();
+                    clearInterval(oInterval);
+                }, 200 );
+            }
+        });
+    },
+
+    /**
+     * @pre oRow esta espandida.
+     */
+    getBindingContextsFilhosRow: function(oRow) {
+        let aBindingContexts = [];
+        let v = this.getView();
+        let oTreeTable = v.byId('treeTable');
+        let iRowIndex = oRow.getIndex();
+        let bc = oTreeTable.getContextByIndex(iRowIndex);
+        let oItem = bc.getObject();
+        for (;;){
+            iRowIndex++;
+            let bc = oTreeTable.getContextByIndex(iRowIndex);
+            if (!bc)
+                break;
+            let oSeguinteItem = bc.getObject();
+            if (oSeguinteItem.Nivel <= oItem.Nivel)
+                break;
+            if (oSeguinteItem.Pai === oItem.Item)
+                aBindingContexts.push(bc);
+        }
+        return aBindingContexts;
+    },
+
+    onChangeReplicarEmFilhos: async function(oEvent) {
+        let oSource = oEvent.getSource();
+        let bc = oSource.getBindingContext();
+        let bIsGenerico = bc.getProperty('IsGenerico');
+        if (!bIsGenerico)
+            return;
+        let sValue = oEvent.getParameters().value;
+        let sRelativePath = oSource.getBinding('value').getPath();
+        let oRow = oSource.getParent().getParent();
+        oSource.setBusy(true);
+        await this.expandRow(oRow);
+        oSource.setBusy(false);
+        let fValue;
+        try {
+            fValue = parseFloat(
+                new Decimal().parseValue(sValue, "string")
+            );
+        } catch (e) {
+            if (e.ParseException == "ParseException")
+                return;
+            throw e;
+        }
+        if (isNaN(fValue))
+            return;
+        let aBindingContexts = this.getBindingContextsFilhosRow(oRow);
+        let m = this.getView().getModel();
+        aBindingContexts
+            .map( bc => bc.getPath() )
+            .forEach( sPath => m.setProperty(`${sPath}/${sRelativePath}`,fValue) );
+    },
+
 });
 
--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,6 +1,9 @@
 import formatter from 'simplifique/telaneg/base/model/formatter';
 
 Object.assign(formatter, {
+    isTipoAbrangenciaCentroRef: function(sTipoAbrangencia) {
+        return sTipoAbrangencia == 'R';
+    },
     exibirInformacoesAtuaisItemsButtonIcon: function(bExibir){
         if (bExibir)
             return "sap-icon://hide";
